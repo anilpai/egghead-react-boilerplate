02c60ace797d9ae4b1be2466c0726d9717acfe3e
Anil Pai <anilbpai@gmail.com>
2019-11-17 22:54:25 -0600

    config prettier to auto format code

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
 
-describe('App', ()=> {
-    it('renders with error', () => {
-        render(<App/>)
-    })
+describe('App', () => {
+  it('renders with error', () => {
+    render(<App />)
+  })
 })
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -2,14 +2,14 @@ const merge = require('webpack-merge')
 const baseConfig = require('./webpack.config.base')
 
 module.exports = merge(baseConfig, {
-    mode: 'development',
-    devServer: {
-        port : 9000
-    },
-    resolve: {
-        alias: {
-            "react-dom": "@hot-loader/react-dom"
-        }
-    },
-    devtool: 'source-map'
-});
+  mode: 'development',
+  devServer: {
+    port: 9000
+  },
+  resolve: {
+    alias: {
+      'react-dom': '@hot-loader/react-dom'
+    }
+  },
+  devtool: 'source-map'
+})
